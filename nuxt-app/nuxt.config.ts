e9a98b3c11a9c49b52a1b8f03abeca2b069c5d4e
T3chy <mads4295kk@gmail.com>
2023-03-16 15:18:28 +0100

    Nevermind new spotify login aproce

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    routeRules: {
+        'api/spotify/login': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+        'http://localhost:3000': { cors: true, headers: { 'Access-Control-Allow-Origin': 'GET' } },
+        'https://localhost:3000/api/spotify/login': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+    },
+    runtimeConfig: {
+        spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
+        spotifyRedirect: process.env.SPOTIFY_REDIRECT
+    }
 })
