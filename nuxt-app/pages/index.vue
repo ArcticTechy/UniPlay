a8e521afef726d99d2674cde10bc5e38d93f7820
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2023-03-30 12:27:30 +0200

    Now play music based on component and function

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -8,12 +8,17 @@
 
         <SpotifyPlayback/>
       </ClientOnly>
+      <AudiusAudio/>
       
+      <input type="text" placeholder="Search...">
+
     </div>
 </template>
 
-
 <script setup lang="ts">
+import AudiusAudio from '~~/components/AudiusAudio.vue';
+
+    // const selectHost = () => window.location.href = "/api/audius/host";
     const LoginSpotify = () => window.location.href = "/api/spotify/login";
     const { $spotifyPlayer } = useNuxtApp();
     
