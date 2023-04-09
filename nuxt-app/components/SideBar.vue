370ce01555cde0c9932810941bdf99125bcb3bfa
T3chy <mads4295kk@gmail.com>
2023-04-09 21:20:16 +0200

    added a button to change the song temp

--- nuxt-app/components/SideBar.vue
+++ nuxt-app/components/SideBar.vue
@@ -5,6 +5,7 @@
 
         </div>
         <div class="userContent">
+            <button @click="changesong" style="background-color: yellow;">test</button>
             <template v-for="(item) in playlists" >
                 <p>{{ item.name }}</p>
             </template>
@@ -31,6 +32,20 @@ getUserPlaylists(accessToken.value)
   .then(Playlists => {
     playlists.value = Playlists
   });
+
+  async function changesong() {
+  const device_id = '8b5acc3fd127d91dc1f6b60a0cafa7f1fadedb42';
+  const track_uri = 'spotify:track:1cDeP3HOdeIQXtBdWCjI4T';
+
+  await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken.value}`
+    },
+    body: JSON.stringify({ uris: [track_uri] })
+  });
+}
 
 
 </script>
