3ac9862cb1bee4b2035b585feee7813499ecd9a4
T3chy <mads4295kk@gmail.com>
2023-04-13 13:59:25 +0200

    We can now load playlist and play them once a song is pressed

---
+++ nuxt-app/layouts/player.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div>
+            <SideBar class="sidebar"></SideBar>
+            <ClientOnly>
+                <Player />
+            </ClientOnly>
+        </div>
+        <div>
+            <slot class="side" />
+        </div>
+    </div>
+</template>
+
+<style>
+
+.sidebar {
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+}
+.container {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    
+}
+</style>
